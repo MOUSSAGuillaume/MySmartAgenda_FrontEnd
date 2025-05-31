c4cbe555554bfe1d07aa6e0e007b455580c07ef8
moussaguillaume <moussaguillaume@hotmail.fr>
2025-06-01 00:13:18 +0200

    début du projet: edit le routage edit index.html et home .html

---
+++ Public/Router/Route.js
@@ -0,0 +1,14 @@
+export default class Route {
+    constructor(url, title, pathHtml,  authorize, pathJS = "") {
+        this.url = url; // L'URL de la route
+        this.title = title; // Le titre de la page
+        this.pathHtml = pathHtml; // Le chemin vers le fichier HTML de la page
+        this.pathJS = pathJS; // Le chemin vers le fichier JavaScript de la page (optionnel)
+        this.authorize = authorize
+    }
+}
+
+/*
+[] -> Tout le monde peut y accéder
+ ["disconnected"] -> Réserver aux utilisateurs déconnecté
+utilisateurs */
